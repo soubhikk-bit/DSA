203613b938c12db8f484ca8c1b661afc146517fa
Soubhik Karmakar <soubhikk04@gmail.com>
2026-08-13 11:59:42 +0000

    Q09 find vowel or consonent solved

---
+++ 00-Logic-Building/01-Conditional-Thinking/Level-01-Simple-Cnditions/Q09_VowelOrConsonent.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Q09_VowelOrConsonent {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a character:");
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+                || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+
+            System.out.println("The character is a vowel");
+
+        } else {
+
+            System.out.println("The character is a consonant");
+        }
+
+        sc.close();
+    }
+}
+
