e8902f8d852919818e27dc16703598729353dc19
Soubhik Karmakar <soubhikk04@gmail.com>
2026-08-18 08:48:22 +0000

    Q09 solved

---
+++ 00-Logic-Building/02-Loops-And-Number-Logic/Level-01-Basic-Looping/Q09_Factorial.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+
+public class Q09_Factorial {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a number");
+
+    int n = sc.nextInt();
+    int fact = 1;
+
+    for(int i = 1; i<=n; i++){
+      fact*=i;
+    }
+    System.out.println(fact);
+    sc.close();
+  }
+}
