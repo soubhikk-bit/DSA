a614101ac4b6be06c348fd6955fffbbc97261686
Soubhik Karmakar <soubhikk04@gmail.com>
2026-08-18 08:39:50 +0000

    Q07 solved

---
+++ 00-Logic-Building/02-Loops-And-Number-Logic/Level-01-Basic-Looping/Q07_SumEvenNumbers.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+
+public class Q07_SumEvenNumbers {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("enter a number");
+    int n = sc.nextInt();
+    int sum = 0;
+
+    for(int i = 1 ; i<= n ; i++ ){
+      if(i%2==0){
+        sum+=i;
+      }
+    }
+    System.out.println(sum);
+    sc.close();
+  }
+}
